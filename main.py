9a87053053007717320d59497db6a7f19b6da6fa
Vivien Giraud <github@viviengiraud.fr>
2022-03-17 10:28:10 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,89 @@
+from bottle import route, run, template, request, response, redirect
+import sqlite3
+import random
+
+
+def generate_cookie_value():
+    return str("".join(random.choice("0123456789ABCDEFabcdef@&!") for i in range(128)))
+
+
+# ' and username = 'admin'; #
+
+
+@route("/user")
+@route("/user/")
+def user_info():
+    fb_session = request.get_cookie("fb_session")
+
+    conn = sqlite3.connect("fb.db")
+    cursor = conn.cursor()
+    sql_r = f"SELECT * FROM facebook WHERE cookie = '{fb_session}'"
+    print(sql_r)
+    cursor.execute(sql_r)
+    result = cursor.fetchone()
+
+    if result is None:
+        redirect("/login")
+
+    return template("user_info", username=result[1], email=result[2])
+
+
+@route("/login", method=["GET", "POST"])
+@route("/login/", method=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return template("login_template")
+    else:
+        username = request.forms.username
+        password = request.forms.password
+
+        conn = sqlite3.connect("fb.db")
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT password FROM facebook WHERE username = '{username}'")
+        db_password = cursor.fetchone()
+
+        if db_password[0] == "":
+            return {"error": True, "message": "Utilisateur inconnu"}
+
+        if db_password[0] != password:
+            return {"error": True, "message": "Mot de passe erroné"}
+
+        cookie_value = generate_cookie_value()
+
+        cursor.execute(
+            f"UPDATE facebook SET cookie = '{cookie_value}' WHERE username = '{username}'"
+        )
+        conn.commit()
+
+        response.set_cookie("fb_session", cookie_value, path="/")
+        redirect("/user/")
+
+
+@route("/signup", method=["GET", "POST"])
+@route("/signup/", method=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return template("signup_template")
+    else:
+        username = request.forms.username
+        email = request.forms.email
+        password = request.forms.password
+        print(username)
+        print(email)
+        print(password)
+        if username == "":
+            return {"error": True, "message": "Il manque le nom d'utilisateur"}
+        conn = sqlite3.connect("fb.db")
+        cursor = conn.cursor()
+        # conn.set_trace_callback(print)
+        sql_request = f"INSERT INTO facebook (username, email, password) VALUES ('{username}', '{email}', '{password}')"
+        print(sql_request)
+        cursor.execute(sql_request)
+        conn.commit()
+        return {
+            "error": False,
+            "message": f"Bien enregistré en tant que {username} id: {cursor.lastrowid}",
+        }
+
+
+run(host="localhost", port=8080, reloader=True)
---
+++ migration.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+print("Creating DB")
+conn = sqlite3.connect("fb.db")
+print("DB created")
+
+conn.execute(
+    """CREATE TABLE facebook (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username char(100) NOT NULL UNIQUE, email char(100) NOT NULL UNIQUE,
+    password char(100) NOT NULL, cookie char(128) UNIQUE)"""
+)
+
+conn.execute(
+    "INSERT INTO facebook (username, email, password) VALUES ('Vivien', 'vivien@facebook.com', 'TEST123@')"
+)
+conn.execute(
+    "INSERT INTO facebook (username, email, password) VALUES ('Elsa', 'elsa@facebook.com', 'hqdfjqdf')"
+)
+conn.execute(
+    "INSERT INTO facebook (username, email, password) VALUES ('Anna', 'anna@facebook.com', 'HDFSI123')"
+)
+
+conn.commit()
+print("Table created")
